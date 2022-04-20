5a95380cb065366985d75f801616ae05d2a98cb6
Cordell Browne <cordell@knights.ucf.edu>
2022-04-20 08:45:37 -0400

    Im not sure about this

---
+++ alpacas/AlpacaTradingView.py
@@ -0,0 +1,31 @@
+import requests, json
+from chalice import Chalice
+
+app = Chalice(app_name='tradingview-webhook-alerts')
+
+API_KEY = 'your-alpaca-api-key'
+SECRET_KEY = 'your-alpaca-secret-key'
+BASE_URL = "https://paper-api.alpaca.markets"
+ORDERS_URL = "{}/v2/orders".format(BASE_URL)
+HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
+
+@app.route('/buy_stock', methods=['POST'])
+def buy_stock():
+    request = app.current_request
+    webhook_message = request.json_body
+
+    data = {
+        "symbol": webhook_message['ticker'],
+        "qty": 1,
+        "side": "buy",
+        "type": "limit",
+        "limit_price": webhook_message['close'],
+        "time_in_force": "gtc",
+        "order_class": "bracket",
+        "take_profit": {
+            "limit_price": webhook_message['close'] * 1.05
+        },
+        "stop_loss": {
+            "stop_price": webhook_message['close'] * 0.98,
+        }
+    }
